fd2f661a3200f7f3a198d2653de89a4a7a59f394
Pavel910 <pavel@webiny.com>
2015-04-27 22:21:39 +0200

    Vertical switch and start of checkbox/radio groups. Syntax example in TaskForm.htpl

--- Apps/Webiny/Backend/UI/Js/Components/Switch/VerticalSwitch.js
+++ Apps/Webiny/Backend/UI/Js/Components/Switch/VerticalSwitch.js
@@ -3,9 +3,20 @@ import BaseInputComponent from '/Webiny/Core/Base/BaseInputComponent';
 class VerticalSwitch extends BaseInputComponent {
 
 	getInitialState() {
-		var state = {};
-		_.set(state, 'css.' + 'col-sm-' + this.props.grid, true);
-		return state;
+		var defaultGrid = {
+			'col-xs-12': true,
+			'col-sm-12': true
+		};
+
+		var gridMap = {
+			6: {
+				'col-xs-6': true,
+				'col-sm-6': true
+			}
+		};
+		return {
+			css: gridMap[this.props.grid] || defaultGrid
+		};
 	}
 
 }
